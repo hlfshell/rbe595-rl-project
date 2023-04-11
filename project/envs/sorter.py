cac9e1f81fb78c50459ff3397692d58d7883ab3c
keith <kchester@gmail.com>
2023-04-11 14:22:11 -0700

    feature: Work in progress - updating environment to have optional pose return instead of images

--- project/envs/sorter.py
+++ project/envs/sorter.py
@@ -1,6 +1,6 @@
 from math import pi
 from random import choice, uniform
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict, Tuple, Callable
 
 import numpy as np
 from panda_gym.envs.core import RobotTaskEnv, Task
@@ -8,14 +8,46 @@ from panda_gym.envs.robots.panda import Panda
 from panda_gym.pybullet import PyBullet
 
 
+class TargetObject:
+    """
+    TargetObject tracks the lifecycle of a target object (not a goal)
+    """
+
+    def __init__(
+        self,
+        id: str,
+        shape: int,
+        position: np.array,
+        color: np.array,
+        removed: bool = False,
+    ):
+        self.id = id
+        self.shape = shape
+        self.position = position
+        self.color = color
+        self.removed = removed
+
+
 class SorterTask(Task):
     def __init__(
         self,
         sim: PyBullet,
+        observation_type: int,
+        robot: Panda,
         objects_count: int = 5,
         img_size: Tuple[int, int] = (256, 256),
     ):
+        if observation_type not in [OBSERVATION_POSES, OBSERVATION_IMAGE]:
+            raise Exception(
+                f"Invalid output type {observation_type}. Must be one of "
+                + f"{OBSERVATION_POSES} for values and {OBSERVATION_IMAGE} "
+                + "four images."
+            )
+
         super().__init__(sim)
+
+        self.robot = robot
+        self.observation_type = observation_type
 
         self.score: float = 0.0
 
@@ -23,8 +55,9 @@ class SorterTask(Task):
         self.steps_remaining = self.initial_steps
         self.steps_sort_extension = 10
 
-        self.objects_count = objects_count
-        self.img_size = img_size
+        self.objects_count: int = objects_count
+        if observation_type == OBSERVATION_IMAGE:
+            self.img_size = img_size
         self.object_opacity = 0.8
 
         self.sim.create_plane(z_offset=-0.4)
@@ -35,18 +68,14 @@ class SorterTask(Task):
         self.sorter_positions: Dict[str, np.array] = {}
         self._init_sorting_areas()
 
-        # objects is a dict w/ integer setting (0-4) for
-        # the five objects created. The resulting dict
-        # contains the "id", "name", initial "position",
-        # "shape", and "color" for each object
-        self.goal: Dict[int, Dict[str, Any]] = {}
+        # Track each target object as part of our goal
+        self.goal: Dict[int, TargetObject] = {}
 
         # Size multiplier is the range of sizes allowed for
         # target objects
         self.size_multiplier: Tuple[float, float] = (0.5, 1.5)
 
         self.task_init()
-
 
     def task_init(self):
         # Create our plane and table for the scenario
@@ -55,14 +84,28 @@ class SorterTask(Task):
         # These position_limits are where the objects are allowed
         # to spawn. This reads as (x, y), where each axis
         # in turn is a tuple of min/max placement.
-        self.object_position_limits: Tuple[Tuple[float, float]] = ((-0.2, 0.2), (-0.2, 0.2))
+        self.object_position_limits: Tuple[Tuple[float, float]] = (
+            (-0.2, 0.2),
+            (-0.2, 0.2),
+        )
+
+        # object_tracking_limits is the range of along each axis
+        # x, y, and z that objects will be tracked for the
+        # OBSERVATION_POSES observation type. Anything beyond this will
+        # be reported as the min/max accordingly. It's defined manually
+        # as essentially a rectangular field in the space.
+        self.object_tracking_limits: Tuple[Tuple[float, float]] = (
+            (-0.5, 0.5),  # x
+            (-0.5, 0.5),  # y
+            (-0.5, 0.5),  # z
+        )
 
     def _init_sorting_areas(self):
         self.sorter_positions = {
             SORTING_ONE: np.array([-0.25, -0.2, 0.01]),
             SORTING_TWO: np.array([-0.25, 0.00, 0.01]),
             SORTING_THREE: np.array([-0.25, 0.2, 0.01]),
-            "blocker": np.array([-0.2, 0.0, 0.01])
+            "blocker": np.array([-0.2, 0.0, 0.01]),
         }
         self.sim.create_box(
             body_name=SORTING_ONE,
@@ -201,7 +244,7 @@ class SorterTask(Task):
                 # against all known objects. If there's
                 # overlap we delete this and move on
                 for other in self.goal:
-                    other_id = self.goal[other]["id"]
+                    other_id = self.goal[other].id
                     if self.check_collision(id, other_id):
                         collisions = True
                         break
@@ -212,13 +255,7 @@ class SorterTask(Task):
                 else:
                     break
 
-            self.goal[object] = {
-                "id": id,
-                "name": name,
-                "position": position,
-                "shape": shape,
-                "color": color,
-            }
+            self.goal[object] = TargetObject(id, shape, position, color)
 
     def check_collision(self, object1: str, object2: str) -> bool:
         """
@@ -230,7 +267,7 @@ class SorterTask(Task):
 
     def delete_all_objects(self):
         for object in self.goal:
-            self.sim.physics_client.removeBody(self.goal[object]["id"])
+            self.sim.physics_client.removeBody(self.goal[object].id)
         self.goal = {}
 
     def get_random_color(self) -> np.array:
@@ -259,8 +296,12 @@ class SorterTask(Task):
         get_random_object_position returns a random np.array of an object's
         permissions within the permissive bounds set at instantiation.
         """
-        x = uniform(self.object_position_limits[0][0], self.object_position_limits[0][1])
-        y = uniform(self.object_position_limits[1][0], self.object_position_limits[1][1])
+        x = uniform(
+            self.object_position_limits[0][0], self.object_position_limits[0][1]
+        )
+        y = uniform(
+            self.object_position_limits[1][0], self.object_position_limits[1][1]
+        )
         z = 0.01
         return np.array([x, y, z])
 
@@ -278,7 +319,18 @@ class SorterTask(Task):
         """
         get_obs will determine if any objects collided and need to be removed,
         and adjust the score as expected. It will then return an observation,
-        which will be the simulation render from the camera angle.
+        which, depending on the observation type, will be:
+            OBSERVATION_POSES: will be a series of values for the raw (x, y, z, theta,
+                phi, psi) pose of the object, as well as an identity (type of shape),
+                and size(0-1) for min/max size, and the pose of the robot's end
+                effector, and a 0-1 value for its gripper open/close state. An
+                example of the return would be:
+                [[x, y, z, theta, phi, psi,
+                  xd, yd, zd, thetad, phid, psid, <~ velocities
+                identity, size] (times # of set objects), ...,
+                (ee_x, ee_y, ee_z, ee_theta, ee_phi, ee_psi),
+                gripper_status]
+            OBSERVATION_IMAGE: will be the simulation render from the camera angle.
         """
         # Decrement our steps so that we can timeout if this takes
         # too long
@@ -288,32 +340,28 @@ class SorterTask(Task):
 
         # First check for floor collisions. Remove any colliding objects.
         floor_id = self.sim._bodies_idx["plane"]
-        clear_keys: List[str] = []
         for object_key in self.goal:
-            object_id = self.goal[object_key]["id"]
+            if self.goal[object_key].removed:
+                continue
+            object_id = self.goal[object_key].id
             if self.check_collision(object_id, floor_id):
                 self.score += FLOOR_PENALTY
                 self.sim.physics_client.removeBody(object_id)
-                clear_keys.append(object_key)
-
-        # Clear up removed goals
-        for key in clear_keys:
-            del self.goal[key]
+                self.goal[object_key].removed = True
 
         # Then check for collisions between the goals and a given target.
         # Remove any colliding targets.
-        clear_keys: List[str] = []
         for object_key in self.goal:
+            if self.goal[object_key].removed:
+                continue
             for goal in GOALS:
                 object = self.goal[object_key]
-                object_id = object["id"]
+                object_id = object.id
                 goal_id = self.sim._bodies_idx[goal]
 
-                if object_key not in clear_keys and self.check_collision(
-                    object_id, goal_id
-                ):
+                if self.check_collision(object_id, goal_id):
                     self.sim.physics_client.removeBody(object_id)
-                    clear_keys.append(object_key)
+                    self.goals[object_key].removed = True
 
                     if CORRECT_SORTS[goal] == object["shape"]:
                         self.score += SORT_REWARD
@@ -322,14 +370,106 @@ class SorterTask(Task):
 
                     self.steps_remaining += self.steps_sort_extension
 
-        # Clear up removed goals
-        for key in clear_keys:
-            del self.goal[key]
-
         # Ensure that each goal hasn't moved; this is a consequence of the
         # collision checking we do
         self.set_sorter_positions()
 
+        if self.observation_type == OBSERVATION_IMAGE:
+            return self._get_img()
+        else:
+            return self._get_poses_output()
+
+    def get_object_pose(self, object: TargetObject) -> np.array:
+        object_position = self.sim.get_base_position("object")
+        object_rotation = self.sim.get_base_rotation("object")
+        object_velocity = self.sim.get_base_velocity("object")
+        object_angular_velocity = self.sim.get_base_angular_velocity("object")
+        observation = np.concatenate(
+            [object_position, object_rotation, object_velocity, object_angular_velocity]
+        )
+        return observation.astype(np.float32)
+
+    def _get_poses_output(self) -> np.array:
+        """
+        _get_poses_output will return the poses of all objects in the scene,
+        as well as their identity and size. It will be a series of values for
+        the raw (x, y, z, theta, phi, psi) pose of the object, as well as an
+        identity (type of shape), and size (0-1) for min/max size, and the pose
+        of the robot's end effector, and a 0-1 value for its gripper open/close
+        state. An example of the return would be:
+
+        [[x, y, z, theta, phi, psi,
+          xd, yd, zd, thetad, phid, psid, <~ velocities
+        identity, size] (times # of set objects), ...,
+        (ee_x, ee_y, ee_z, ee_theta, ee_phi, ee_psi,
+        ee_xd, ee_yd, ee_zd, ee_thetad, ee_phid, ee_psid), # <~ velocities
+        gripper_status]
+        """
+        # The size of this vector is determined by the number of objects expected
+        pose_values = 12
+        # The length of our vector is (pose_values + 2 (identity, size)) for each
+        # object, pose_values for the end effector, and one additional value for
+        # the gripper finger state (distance between fingers)
+        size = (len(self.goal) * (pose_values + 2)) + pose_values + 1
+        observation: np.array = np.zeros((size, 1), dtype="float32")
+
+        index = 0
+        for object in self.goal.values():
+            # If the object has been removed, just report 0's for its existence
+            if object.removed:
+                index += 1
+                continue
+
+            pose = self.get_object_pose(object)
+
+            # Convert the (x,y,z) position to (0,1) range with then
+            pose[0:3] = self._convert_position_to_within_bounds(pose[0:3])
+
+            observation[index * 12 : (index + 1) * 12] = pose
+            index += 1
+
+        # Get the end effector position
+        ee_position = self.robot.get_ee_position()
+        ee_position = self._convert_position_to_within_bounds(ee_position)
+        ee_rotation = 0.0
+        ee_velocity = self.robot.get_ee_velocity()
+        ee_angulary_velocity = 0.0
+        fingers_width = self.robot.get_fingers_width()
+        ee_index = index * 12 * len(self.goal)
+        observation[ee_index : ee_index + 3] = ee_position
+        observation[ee_index + 3 : ee_index + 6] = ee_rotation
+        observation[ee_index + 6 : ee_index + 9] = ee_velocity
+        observation[ee_index + 9 : ee_index + 12] = ee_angulary_velocity
+        observation[ee_index + 12] = fingers_width
+
+        return observation
+
+    def _convert_position_to_within_bounds(self, position: np.array) -> np.array:
+        """
+        _convert_position_to_within_bounds will convert the position to be
+            relative within the bounds of the object tracking limits in the
+            range of (0,1)
+        """
+        position[0] = max(
+            self.object_tracking_limits[0][0],
+            min(position[0], self.object_tracking_limits[0][1]),
+        )
+        position[1] = max(
+            self.object_tracking_limits[1][0],
+            min(position[1], self.object_tracking_limits[1][1]),
+        )
+        position[2] = max(
+            self.object_tracking_limits[2][0],
+            min(position[2], self.object_tracking_limits[2][1]),
+        )
+
+        return position.astype(np.float32)
+
+    def _get_img(self) -> np.array:
+        """
+        _get_img will return the image from the camera in human rendering
+        mode
+        """
         # We have to swap render mode if it's set to human mode
         # to get it to draw for us.
         original_render_mode = self.sim.render_mode
@@ -348,7 +488,9 @@ class SorterTask(Task):
         return img
 
     def get_achieved_goal(self) -> np.ndarray:
-        return np.array([len(self.goal) < 0], dtype="bool")
+        return np.array(
+            all(target.removed for target in self.goal.values()), dtype="bool"
+        )
 
     def is_terminated(self) -> bool:
         """
@@ -362,7 +504,7 @@ class SorterTask(Task):
         if self.steps_remaining <= 0:
             return True
 
-        return len(self.goal) <= 0
+        return all(obj.removed for obj in self.goal.values())
 
     def is_success(
         self,
@@ -401,6 +543,7 @@ class SorterEnv(RobotTaskEnv):
 
     def __init__(
         self,
+        observation_type: int,
         objects_count: int = 5,
         render_mode: str = "human",
         control_type: str = "ee",
@@ -408,6 +551,9 @@ class SorterEnv(RobotTaskEnv):
         render_width: int = 720,
         render_height: int = 480,
     ) -> None:
+        if observation_type not in [OBSERVATION_IMAGE, OBSERVATION_POSES]:
+            raise ValueError("observation_type must be one of either images or poses")
+
         sim = PyBullet(
             render_mode=render_mode,
             background_color=np.array((200, 200, 200)),
@@ -419,7 +565,12 @@ class SorterEnv(RobotTaskEnv):
             base_position=np.array([-0.6, 0.0, 0.0]),
             control_type=control_type,
         )
-        task = SorterTask(sim, objects_count=objects_count)
+        task = SorterTask(
+            sim,
+            observation_type,
+            robot.get_ee_position,
+            objects_count=objects_count,
+        )
         super().__init__(
             robot,
             task,
@@ -451,9 +602,6 @@ class SorterEnv(RobotTaskEnv):
         }
 
 
-
-
-
 SORTING_ONE = "sorting_one"
 SORTING_TWO = "sorting_two"
 SORTING_THREE = "sorting_three"
@@ -475,3 +623,6 @@ STEP_PENALTY = -1
 FLOOR_PENALTY = -50
 WRONG_SORT_REWARD = 25
 SORT_REWARD = 100
+
+OBSERVATION_POSES: int = 0
+OBSERVATION_IMAGE: int = 1
