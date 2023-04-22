588f859adcf4a7a50fe3b92652eb0b4ba95026e6
keith <kchester@gmail.com>
2023-04-22 10:17:23 -0700

    debug: wip trying to test diff model approaches to isolate bad training

--- test_train.py
+++ test_train.py
@@ -3,7 +3,12 @@ from project.envs.tosser import TosserEnv
 from project.ppo.pose_memory import TrainingState
 from project.ppo.pose_trainer import Trainer
 from project.ppo.trainer_with_memory import Trainer as MemoryTrainer
-from project.ppo.pose_model import PPOPoseActor, PPOPoseCritic2, PPOPoseActorMV
+from project.ppo.pose_model import (
+    PPOPoseActor,
+    PPOPoseCritic2,
+    PPOPoseActorMV,
+    PPOPoseCritic,
+)
 
 import os
 
@@ -11,7 +16,8 @@ env = SorterEnv(OBSERVATION_POSES, 5, render_mode="rgb_array")
 
 # actor = PPOPoseActor(5, "ee")
 actor = PPOPoseActorMV(5, "ee")
-critic = PPOPoseCritic2(5, "ee")
+critic = PPOPoseCritic(5, "ee")
+# critic = PPOPoseCritic2(5, "ee")
 
 # Determine if there is a training state at training/state.data -
 # if so, load the TrainingState for it. Otherwise, create a new one.
@@ -32,9 +38,10 @@ trainer = Trainer(
     env,
     actor,
     critic,
-    timesteps = 200_000_000,
-    timesteps_per_batch= 10_000,
-    max_timesteps_per_episode=250
+    timesteps=200_000_000,
+    timesteps_per_batch=10_000,
+    # timesteps_per_batch=1_000,
+    max_timesteps_per_episode=250,
 )
 
 trainer.train()
