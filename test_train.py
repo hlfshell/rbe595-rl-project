0bfae717127e475e3a45152f6579217b65382c44
Keith Chester <kchester@gmail.com>
2023-04-23 10:20:32 -0700

    feature: check for training dir before starting

--- test_train.py
+++ test_train.py
@@ -1,47 +1,31 @@
-from project.envs.sorter import SorterEnv, OBSERVATION_IMAGE, OBSERVATION_POSES
-from project.envs.tosser import TosserEnv
-from project.ppo.pose_memory import TrainingState
+from project.envs.sorter import SorterEnv, OBSERVATION_POSES
 from project.ppo.pose_trainer import Trainer
-from project.ppo.trainer_with_memory import Trainer as MemoryTrainer
 from project.ppo.pose_model import (
     PPOPoseActor,
-    PPOPoseCritic2,
-    PPOPoseActorMV,
     PPOPoseCritic,
 )
 
-import os
+from os import path
+from pathlib import Path
 
 env = SorterEnv(OBSERVATION_POSES, 5, render_mode="rgb_array")
 
-# actor = PPOPoseActor(5, "ee")
-actor = PPOPoseActorMV(5, "ee")
+actor = PPOPoseActor(5, "ee")
 critic = PPOPoseCritic(5, "ee")
-# critic = PPOPoseCritic2(5, "ee")
-
-# Determine if there is a training state at training/state.data -
-# if so, load the TrainingState for it. Otherwise, create a new one.
-# Check if training/state.data exists
-# if os.exists("./training/state.data"):
-#     state = TrainingState.Load("./training/state.data")
-# else:
-#     state = TrainingState(10_000, 1_000_000, 4800, 5, 5)
-
-# trainer = MemoryTrainer(
-#     env,
-#     actor,
-#     critic,
-#     TrainingState(10_000, 1_000_000, 4800, 5, 5),
-# )
 
 trainer = Trainer(
     env,
     actor,
     critic,
     timesteps=200_000_000,
-    timesteps_per_batch=10_000,
-    # timesteps_per_batch=1_000,
-    max_timesteps_per_episode=250,
+    timesteps_per_batch=5_000,
+    max_timesteps_per_episode=750,
 )
+
+# Determine if there is a training state at training/state.data -
+# if so, load the TrainingState for it. Otherwise, create a new one.
+Path("./training").mkdir(parents=True, exist_ok=True)
+if path.isfile("./training/state.data"):
+    trainer.load("./training")
 
 trainer.train()
