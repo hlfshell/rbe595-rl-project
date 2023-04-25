4cd0705bad7d7dd7556b2d048b8d58439503e9bc
Keith Chester <kchester@gmail.com>
2023-04-24 20:31:49 -0700

    experimenting

--- test_pendulum.py
+++ test_pendulum.py
@@ -6,7 +6,7 @@ from project.ppo.pose_model import (
 
 import gymnasium as gym
 
-env = gym.make('Pendulum-v1', render_mode="human")
+env = gym.make('Pendulum-v1')
 
 actor = PPOPoseActor(5, "pendulum")
 critic = PPOPoseCritic(5, "pendulum")
@@ -16,8 +16,18 @@ trainer = Trainer(
     actor,
     critic,
     timesteps=200_000_000,
-    timesteps_per_batch=500,
-    max_timesteps_per_episode=100,
+    timesteps_per_batch=2048,
+    max_timesteps_per_episode=200,
 )
+	# hyperparameters = {
+	# 			'timesteps_per_batch': 2048, 
+	# 			'max_timesteps_per_episode': 200, 
+	# 			'gamma': 0.99, 
+	# 			'n_updates_per_iteration': 10,
+	# 			'lr': 3e-4, 
+	# 			'clip': 0.2,
+	# 			'render': True,
+	# 			'render_every_i': 10
+	# 		  }
 
 trainer.train()
